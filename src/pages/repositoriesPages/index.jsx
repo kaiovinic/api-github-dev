67c15494f96bcda077500ea57f68b5cc77791eff
kaiovinic <kaiovini19@hotmail.com>
2024-07-06 23:56:34 -0300

    Carregando informações de repositorios

--- src/pages/repositoriesPages/index.jsx
+++ src/pages/repositoriesPages/index.jsx
@@ -3,10 +3,12 @@ import { Loading, Container, Sidebar, Main } from "./styles";
 import Profile from "./Profile";
 import Filter from "./Filter";
 import Repositories from "./Repositories";
-import { getLangsFrom, getUser } from "../../services/api";
+import { getUser, getRepos, getLangsFrom } from "../../services/api";
 
 function RepositoriesPages() {
   const [user, setUser] = useState();
+  const [repositories, setRepositories] = useState();
+  const [languages, setLanguages] = useState();
   const [currentLanguage, setCurrentLanguage] = useState();
   const [loading, setLoading] = useState(true);
 
@@ -14,72 +16,20 @@ function RepositoriesPages() {
     const loadData = async () => {
       // getUser();
 
-      const [userResponse] = await Promise.all([getUser("kaiovinic")]);
+      const [userResponse, repositoriesResponse] = await Promise.all([
+        getUser("kaiovinic"),
+        getRepos("kaiovinic"),
+      ]);
 
       setUser(userResponse.data);
+      setRepositories(repositoriesResponse.data);
+
+      setLanguages(getLangsFrom(repositoriesResponse.data));
 
       setLoading(false);
     };
     loadData();
   }, []);
-
-  // const user = {
-  //   login: "kaiovinic",
-  //   name: "Kaio",
-  //   avatar_url: "https://avatars.githubusercontent.com/u/105761498?v=4",
-  //   followers: 0,
-  //   following: 0,
-  //   company: null,
-  //   blog: "",
-  //   location: "SALVADOR-BA",
-  // };
-
-  const repositories = [
-    {
-      id: " 1",
-      name: "Repo 1",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "JavaScript",
-    },
-    {
-      id: "2",
-      name: "Repo 2",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "JavaScript",
-    },
-    {
-      id: " 3",
-      name: "Repo 3",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "Golang",
-    },
-    {
-      id: "4",
-      name: "Repo 4",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "Node",
-    },
-    {
-      id: " 5",
-      name: "Repo 5",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "TypeScript",
-    },
-    {
-      id: "6",
-      name: "Repo 6",
-      description: "Descrição",
-      html_url: "https://devsamurai.com.br",
-      language: "C#",
-    },
-  ];
-
-  const languages = getLangsFrom(repositories);
 
   const onFilterClick = (language) => {
     setCurrentLanguage(language);
